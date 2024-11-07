8c87ef618824edc06c4ea0b3aa7829ef3e38d90b
Nwokocha Maruche <nwokochafranklyn@gmail.com>
2024-11-07 11:45:45 +0100

    Fix: reworked the whitelist page side bar

---
+++ assets/js/script2.js
@@ -0,0 +1,19 @@
+
+const forms = document.forms
+
+const initialize=()=>{
+    // Initializes the whitelist page on pageLoad
+
+}
+
+const showSecondForm=()=>{
+    // Shows the second form
+}
+
+const showModal=()=>{
+    // Displays the modal after completion
+}
+
+const sendEmail=()=>{
+    // forwards the form data to the email provided
+}
