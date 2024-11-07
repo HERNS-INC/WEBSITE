ce706fcc896a8ef7b1252b37451a0f5c62164df2
Nwokocha Maruche <nwokochafranklyn@gmail.com>
2024-11-07 15:13:18 +0100

    Action: added interaction to the whitelist page by implementing some javascript functions that handle dom manipulations

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,9 +1,25 @@
 
 const forms = document.forms
+const nigeriaRadio = document.getElementById('nigeria-radio')
+const americaRadio = document.getElementById('america-radio')
+const sideBarItems = document.getElementsByClassName('sideBarItem')
+
+
+nigeriaRadio.addEventListener('select', ()=>{
+    switchCountry('nigeria');
+})
+
+americaRadio.addEventListener('select', ()=>{
+    switchCountry('america')
+})
+
 
 const initialize=()=>{
     // Initializes the whitelist page on pageLoad
+}
 
+const nextForm=()=>{
+    // Go to the next form
 }
 
 const showSecondForm=()=>{
@@ -16,4 +32,17 @@ const showModal=()=>{
 
 const sendEmail=()=>{
     // forwards the form data to the email provided
+}
+
+const switchCountry=(country)=>{
+    switch(country){
+        case "nigeria":
+            for (let item in sideBarItems){
+                item.classList.toggle('showSideBarItem')
+            }
+        case "america":
+            for (let item in sideBarItems){
+                item.classList.toggle('hideSideBarItem')
+            }
+    }
 }
