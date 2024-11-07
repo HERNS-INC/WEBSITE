c7fd77d90f8a506631342d2effd869d2f7b26634
Nwokocha Maruche <nwokochafranklyn@gmail.com>
2024-11-07 16:20:38 +0100

    Styling: styled the whitelist-form-2

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,25 +1,46 @@
 
-const forms = document.forms
+const form1 = document.getElementById('whitelist-form-1')
+const form2 = document.getElementById('whitelist-form-2')
 const nigeriaRadio = document.getElementById('nigeria-radio')
 const americaRadio = document.getElementById('america-radio')
 const sideBarItems = document.getElementsByClassName('sideBarItem')
+const sideBarListItemTwo = document.getElementById('whitelist-item-2')
+const sideBarDashedLine = document.getElementById('sideBarDashedLine')
 
+const body = document.body
+nigeriaRadio.check = true;
 
-nigeriaRadio.addEventListener('select', ()=>{
+nigeriaRadio.addEventListener('click', ()=>{
     switchCountry('nigeria');
 })
 
-americaRadio.addEventListener('select', ()=>{
+americaRadio.addEventListener('click', ()=>{
     switchCountry('america')
 })
 
 
 const initialize=()=>{
     // Initializes the whitelist page on pageLoad
+    nigeriaRadio.check = true;
 }
 
 const nextForm=()=>{
-    // Go to the next form
+    /*Go to the next form
+
+        - Hides form 1 when the next button is selected
+        - Displays form two
+    */
+    form1.style.display = 'none'
+    form2.style.display = 'block'
+}
+const previousForm=()=>{
+     /*Go to the previous form
+
+        - Hides form 2 when the backward button is selected
+        - Displays form one
+    */
+    form1.style.display = 'block'
+    form2.style.display = 'none'
 }
 
 const showSecondForm=()=>{
@@ -35,14 +56,11 @@ const sendEmail=()=>{
 }
 
 const switchCountry=(country)=>{
-    switch(country){
-        case "nigeria":
-            for (let item in sideBarItems){
-                item.classList.toggle('showSideBarItem')
-            }
-        case "america":
-            for (let item in sideBarItems){
-                item.classList.toggle('hideSideBarItem')
-            }
-    }
+   if (country==="nigeria"){
+    sideBarDashedLine.style.visibility ='visible'
+    sideBarListItemTwo.style.visibility='visible'
+   }else{
+    sideBarDashedLine.style.visibility = 'hidden'
+    sideBarListItemTwo.style.visibility = 'hidden'
+   }
 }
