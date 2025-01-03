0fbd42a20606f987cc26a52eb7a0ba83c98711bb
HERNS-INC <162850503+HERNS-INC@users.noreply.github.com>
2025-01-03 14:19:18 -0800

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('menu-toggle');
-            const navbar = document.getElementById('navbar-2');
-
-            toggle.addEventListener('click', () => {
-                navbar.classList.toggle('active');
-            });
